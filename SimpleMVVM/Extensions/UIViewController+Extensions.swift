de29d8f0cbb498aab4f3d6fe1a74a81906c74f40
Hosein Rezazadeh <hrzjanati@gmail.com>
2021-12-28 00:19:22 +0330

    Hossein Rezazadeh Janati 28 December 2021

--- SimpleMVVM/Extensions/UIViewController+Extensions.swift
+++ SimpleMVVM/Extensions/UIViewController+Extensions.swift
@@ -5,4 +5,12 @@
 //  Created by 𝙷𝚘𝚜𝚎𝚒𝚗 𝙹𝚊𝚗𝚊𝚝𝚒  on 12/27/21.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    func showAlert( _ message: String ) {
+        let alert = UIAlertController(title: "", message: message, preferredStyle: .alert)
+        alert.addAction( UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+}
